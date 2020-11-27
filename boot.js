3aa2b834882b198914ee79880ba004ca450d127c
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2020-11-27 10:50:06 +0530

    updating

--- boot.js
+++ boot.js
@@ -1,33 +1,17 @@
 var log = require('logger')('serandives:boot');
 var nconf = require('nconf').argv().env();
-var fs = require('fs');
-var path = require('path');
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-var sera = require('sera');
-var server = require('./index');
 
 var env = nconf.get('ENV');
 
 nconf.defaults(require('./env/' + env + '.json'));
 
+var serandives = require('./index');
+
 var mongourl = nconf.get('MONGODB_URI');
 var ssl = !!nconf.get('MONGODB_SSL');
-
-var initScripts = fs.readdirSync(path.join(__dirname, 'initializers', 'scripts'));
-
-var serandivesScripts = function () {
-  var scripts = [];
-  initScripts.forEach(function (name) {
-    scripts.push({
-      name: name,
-      initializer: require('./initializers/scripts/' + name)
-    });
-  });
-  return scripts;
-};
 
 mongoose.connect(mongourl, {
   authSource: 'admin',
@@ -42,14 +26,17 @@ db.on('error', function (err) {
 
 db.once('open', function () {
   log.info('db:opened');
-  server.prepare(function (err) {
+  serandives(function (err) {
     if (err) {
-      return done(err);
+      return log.error('initialize:serandives', err);
     }
-    sera.boot(serandivesScripts(), function (err) {
+    serandives.boot(function (err) {
+      if (err) {
+        return log.error('initialize:boot', err);
+      }
       mongoose.disconnect(function () {
         if (err) {
-          return log.error('initializers:errored', err);
+          return log.error('initialize:database', err);
         }
         log.info('db:initialized');
       });
